f91f9dff27f39251d6c64d2d38043a93f0fc662d
jeongkijeong <jjung6820@gmail.com>
2022-11-28 13:25:41 +0900

    add objectToJsonStr add jsonStrToObject

--- src/main/java/com/mlog/util/Utils.java
+++ src/main/java/com/mlog/util/Utils.java
@@ -1,12 +1,16 @@
 package com.mlog.util;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.lang.management.ManagementFactory;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 
 public class Utils {
 
@@ -68,30 +72,66 @@ public class Utils {
             return null;
         }
 
-        String jsonStr = null;
+        String result = null;
         try {
             Gson gson = new GsonBuilder().create();
-            jsonStr = gson.toJson(object);
+            result = gson.toJson(object);
         } catch (Exception e) {
             e.getStackTrace();
         }
 
-        return jsonStr;
+        return result;
     }
 
-    public static Object jsonStrToObject(String jsonStr) {
-        if (jsonStr == null) {
+    public static Object jsonStrToObject(String result) {
+        if (result == null) {
             return null;
         }
 
         Object object = null;
         try {
             Gson gson = new GsonBuilder().create();
-            object = gson.fromJson(jsonStr, new HashMap<String, Object>().getClass());
+            object = gson.fromJson(result, new HashMap<String, Object>().getClass());
         } catch (Exception e) {
             e.getStackTrace();
         }
 
         return object;
     }
+
+    public static int getProcessId() {
+        int processId = -1;
+
+        String name = ManagementFactory.getRuntimeMXBean().getName();
+        if (name != null) {
+            processId = Integer.valueOf(name.split("@")[0]);
+        }
+
+        return processId;
+    }
+
+    public static String fileToString(String path) {
+        String result = "";
+
+        File file = new File(path);
+        if (!file.exists()) {
+            return result;
+        }
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file));) {
+            StringBuilder sb = new StringBuilder();
+
+            String temp;
+            while ((temp = br.readLine()) != null) {
+                sb.append(temp);
+            }
+
+            result = sb.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
 }
